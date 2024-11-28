8041021907f2583711ba47921a0b90b78964fcfa
henry dos anjos da luz <henry.anjosdaluz@gmail.com>
2024-11-28 11:07:20 -0300

    versão sem respostas certas ou erradas

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ let alternativa_certa = document.querySelector("#alternativa_certa")
 let alternativa4 = document.querySelector("#alternativa4")
 let alternativa5 = document.querySelector("#alternativa5")
 let botao_responder = document.querySelector("#botao-responder")
-
+let contagem_alternativa1 = 0
+let contagem_alternativa2 = 0
+let contagem_alternativa3 = 0
+let contagem_alternativa4 = 0
+let contagem_alternativa5 = 0
 
 
 alternativa1.addEventListener("click", () =>{ alternativa1.classList.add('selecionada')})
@@ -15,21 +19,25 @@ alternativa4.addEventListener("click", () =>{ alternativa4.classList.add('seleci
 alternativa5.addEventListener("click", () =>{ alternativa5.classList.add('selecionada')})
 
 
+/*
+funcionalidade pré programada para selecionar acertos e erros 
 botao_responder.addEventListener("click", () => {
     alternativa1.classList.add('resposta-errada')
     alternativa2.classList.add('resposta-errada')
     alternativa4.classList.add('resposta-errada')
     alternativa5.classList.add('resposta-errada')
     alternativa_certa.classList.add('resposta-certa')
-
 }
 )
+*/
 
-let contagem_acertos = 0
-nota_final
-function nota_final(){
-    if (alternativa_certa = "click" ){
-        contagem_acertos+=1
-        console.log(contagem_acertos)
+
+
+
+/*função para contar o número de respostas e  quais respostas */ 
+function contar (){
+    if (alternativa1 = "click"){
+        contagem_alternativa1 +=1
+        console.log (contagem_alternativa1)
     }
 }
